7aa2b90d81ce9b03ce157a1f3e316d294db9b53c
Alexandr Korobov <devarko@sapo.pt>
2022-12-17 16:47:06 +0300

    Profile page & url

--- core/settings.py
+++ core/settings.py
@@ -108,6 +108,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Auth
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
