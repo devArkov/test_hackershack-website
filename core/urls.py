3b772de25234276815e33cf270da0ebbb08fb4a7
Alexandr Korobov <devarko@sapo.pt>
2022-12-17 14:06:26 +0300

    url fix

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path\
+from django.urls import path
+from django.views.generic.base import TemplateView
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="home"),
+    path('', TemplateView().as_view(template_name='index.html'), name="home"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse('Homepage')
+    return HttpResponse(template.render({}, request))
